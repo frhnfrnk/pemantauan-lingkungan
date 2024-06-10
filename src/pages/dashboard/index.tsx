fdc450170f4bfe42c5fadaad0ad87e926a4ba525
Joy Ivan <ivanoskytan@gmail.com>
2024-06-10 22:54:54 +0700

    feat: add leaflet as monitoring map

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import Panel from "@/components/Panel";
 import { useRouter } from "next/router";
+import { MapContainer,TileLayer, Marker, Popup } from "react-leaflet";
 
 
 export default function Dashboard() {
@@ -11,6 +12,12 @@ export default function Dashboard() {
     return (
         <div className="flex">
             <Panel />
+            <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />    
+            </MapContainer>
         </div>
     )
 }
