69157d66d793423805e9cf4f5a8d0865a1ef381a
rhafif-p <rhafif.prasetyo@mail.ugm.ac.id>
2024-06-11 15:02:18 +0700

    feat: user location

---
+++ src/components/MapMenu copy.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const MapMenu = ({mapRef, userPosition}) => {
+
+    const map = mapRef.current;
+
+    const handleFly = () => {
+        map.flyTo([-7.819405, 110.357586], 12)
+    }
+
+    const handleFlyUser = () => {
+        map.flyTo(userPosition, 16)
+    }
+
+
+
+    return (
+        <div className="absolute w-52 h-16 right-5 top-0 flex justify-end items-center gap-5">
+            <div className="bg-white h-10 w-10 rounded-md z-50 cursor-pointer flex items-center justify-center drop-shadow-md hover:bg-slate-100" onClick={() => handleFly()}>
+                <img src="/zoom_out.svg" alt="Icon" className="w-8 h-8" />            
+            </div>
+            <div className="bg-white h-10 w-10 rounded-md z-50 cursor-pointer flex items-center justify-center drop-shadow-md hover:bg-slate-100" onClick={() => handleFlyUser()}>
+                <img src="/location.svg" alt="Icon" className="w-8 h-8" />            
+            </div>
+      </div>
+    );
+}
+
+export default MapMenu;
